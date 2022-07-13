48dbb2f8967de35ea4b0848cb0403682e0bb747a
Mitali Gupta <78539161+Mitali2700@users.noreply.github.com>
2022-07-13 06:45:10 +0000

    commit 2

---
+++ index.js
@@ -0,0 +1,13 @@
+function add(num1,num2){
+  return num1+num2;
+}
+
+function multiply(num1,num2){
+  return num1*num2;
+}
+
+function calculator(num1,num2){
+  return operator(num1+num2);
+}
+
+calculator(4,5,add);
